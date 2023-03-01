5909a065603222413ca0c5cb0534dbe341f562c2
chuahce <choongeng.chuah@gmail.com>
2023-03-01 18:37:55 +0800

    Initial Commit

---
+++ src/test/java/nusiss/tfip/ssf/pizza/PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package nusiss.tfip.ssf.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
